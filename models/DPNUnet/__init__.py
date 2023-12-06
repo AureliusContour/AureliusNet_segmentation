71834691f4fa7da10b0a59b382f4ef69aad9ab02
Edem Gold <78963277+EdemGold@users.noreply.github.com>
2023-12-06 17:48:53 +0100

    Update __init__.py to import losses class and metric function

--- models/DPNUnet/__init__.py
+++ models/DPNUnet/__init__.py
@@ -1,6 +1,9 @@
+
 from .DPNUnet import DPNUnet
 from .components import *
+#import from losses_and_metrics.py to  make it accessible
+from .losses_and_metrics.py import *
 
 __all__ = [
-	"DPNUnet", "components"
+	"DPNUnet", "components", "DiceLoss", "calculate_confidence"
 ]
