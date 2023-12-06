494bad30bee4a4ac2d5251fdd1d4d952d57b8a3f
Edem Gold <78963277+EdemGold@users.noreply.github.com>
2023-12-07 00:49:59 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,5 @@
 #utils/losses_and_metrics.py
+from .active_learning.active_learning_utils import *
+from .losses_and_metrics.losses_and_metrics_utils import DiceClass, calculate_confidence 
+
+__all__ = ["DiceLoss", "calculate_confidence"]
