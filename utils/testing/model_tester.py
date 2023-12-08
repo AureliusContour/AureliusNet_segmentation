2b0591bc45d5847d633590ec36345b73e651eb59
Jonathan Bastine Kho <jonathanbastine@gmail.com>
2023-12-08 12:43:51 +0800

    Add Docs and example of usage

--- utils/testing/model_tester.py
+++ utils/testing/model_tester.py
@@ -9,16 +9,55 @@ import os
 
 
 class ModelTester:
-    def __init__(self, model: torch.nn.Module, metrics, device=DEVICE) -> None:
+    """
+    Class for evaluating and predicting DPN U net model
+
+    Args:
+    - model (torch.nn.Module): Segmentation model to be tested
+    - metrics (List[Callable[[torch.Tensor, torch.Tensor], torch.Tensor]]): List of metrics to evaluate the model
+    - device (torch.device, optional): Device to use for testing (default is `DEVICE` from the config)
+
+    Examples:
+    ```
+    # Example to evaluate
+    # Loading Model
+    model = DPNUnet()
+    tester = ModelTester(model=model, metrics=[dice_score])
+    tester.load_checkpoint("output/weights/best.pt")
+
+    # Prepare dataset
+    df = pd.read_csv(DATASET_FILENAMES_SPLIT)
+    for col in ['ct_file', 'mask_file', 'resized_ct_file', 'resized_mask_file']:
+        df[col] = df[col].apply(lambda x: os.path.join(DATASET_PATH, x))
+    
+    transform = A.Compose([
+        ToTensorV2(),
+    ], is_check_shapes=False)
+    test_dataset = BreastCTDataset(dataframe=df, set_type="test", transform=transform)
+
+    # Evaluate
+    tester.evaluate(test_dataset, batch_size=BATCH_SIZE)
+    ```
+    """
+    def __init__(self, model: torch.nn.Module, metrics:list, device=DEVICE) -> None:
         self.__model = model.to(device)
         self.__metrics = metrics
         self.__device = device
 
-    def load_checkpoint(self, path):
+    def load_checkpoint(self, path:str):
         checkpoint = torch.load(path)
         self.__model.load_state_dict(checkpoint['model_state_dict'])
 
-    def predict(self, image):
+    def predict(self, image: torch.Tensor) -> torch.Tensor:
+        """
+        Make predictions using the model.
+
+        Args:
+        - image (torch.Tensor): Input image tensor
+
+        Returns:
+        - torch.Tensor: Predicted mask image tensor
+        """
         self.__model.eval()
 
         image = image.to(self.__device)
@@ -28,7 +67,14 @@ class ModelTester:
         
         return output
 
-    def visualize_result(self, image, predicted_mask):
+    def visualize_result(self, image:np.ndarray, predicted_mask:torch.Tensor):
+        """
+        Visualize the original image, predicted mask, and combined result
+
+        Args:
+        - image (numpy.ndarray): Original image
+        - predicted_mask (torch.Tensor): Predicted mask
+        """
         plt.figure(figsize=(18, 6))
         mask = np.where(predicted_mask.squeeze().detach().numpy() < SEGMENTATION_THRESHOLD, 0, 255)
 
@@ -49,7 +95,15 @@ class ModelTester:
 
         plt.show()
 
-    def evaluate(self, dataset, save_folder_path=None, batch_size=BATCH_SIZE):
+    def evaluate(self, dataset:torch.utils.data.Dataset, save_folder_path:str=None, batch_size:int=BATCH_SIZE):
+        """
+        Evaluate the model on a dataset.
+
+        Args:
+        - dataset (torch.utils.data.Dataset): Dataset for evaluation
+        - save_folder_path (str, optional): Path to save predicted masks (default is not saved)
+        - batch_size (int, optional): Batch size for evaluation (default is `BATCH_SIZE` from the config)
+        """
         self.__model.eval()
         data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=os.cpu_count(), pin_memory=True)
         
