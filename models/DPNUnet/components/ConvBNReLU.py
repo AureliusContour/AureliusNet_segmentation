implement gradient checkpoints and fixed model arch

models/DPNUnet/components/ConvBNReLU.py
@@ -3,14 +3,14 @@ from torch import nn
 
 # 3x3 conv + BN + ReLU
 class ConvBNReLU(nn.Module):
-	def __init__(self, in_ch, out_ch):
+	def __init__(self, in_ch:int, out_ch:int):
 		"""
 		A module composing of 3x3 Conv + Batch Normalization + ReLU
 		"""
 		super(ConvBNReLU, self).__init__()
 		self.conv = nn.Conv2d(in_ch,out_ch, kernel_size=3, padding=1)
 		self.bn = nn.BatchNorm2d(out_ch)
-		self.relu = nn.ReLU()
+		self.relu = nn.ReLU(inplace=True)
 		
 	def forward(self, x):
 		x = self.conv(x)

models/DPNUnet/components/DPNBlock/components/BNReLU1Conv.py
@@ -3,13 +3,13 @@ from torch import nn
 
 # BN + ReLU + 1x1 conv
 class BNReLU1Conv(nn.Module):
-    def __init__(self, in_ch, out_ch):
+    def __init__(self, in_ch:int, out_ch:int):
         """
         A module composing of Batch Normalization + ReLU + 1x1 Conv
         """
         super(BNReLU1Conv, self).__init__()
         self.bn = nn.BatchNorm2d(in_ch)
-        self.relu = nn.ReLU()
+        self.relu = nn.ReLU(inplace=True)
         self.conv = nn.Conv2d(in_ch,out_ch, kernel_size=1, padding=0)
 		
     def forward(self, x):

models/DPNUnet/components/DownLayer.py
@@ -1,10 +1,11 @@
 # Library dependencies
 from torch import nn
 from .DPNBlock import DPNBlock
+import torch.utils.checkpoint as checkpoint
 
 # Down Layer (DPN Block)
 class DownLayer(nn.Module):
-	def __init__(self, in_ch, size_conv_a, size_conv_b, size_conv_c, split_size, num_of_blocks):
+	def __init__(self, in_ch, size_conv_a, size_conv_b, size_conv_c, split_size, num_of_blocks, checkpoint=False):
 		"""
 		A Down Layer that composes of an initial 3x3 Max Pooling (stride 2),
 		followed by a number of DPN Blocks that takes in the arguments:
@@ -19,12 +20,23 @@ class DownLayer(nn.Module):
 		self.num_of_blocks = num_of_blocks
 		self.max_pooling = nn.MaxPool2d(3, stride=2, padding=1)
 		self.initDPN = DPNBlock(in_ch, size_conv_a, size_conv_b, size_conv_c, split_size)
-		self.nextDPN = DPNBlock(size_conv_c + split_size, size_conv_a, size_conv_b, size_conv_c, split_size)
+		self.nextDPN = nn.ModuleList([
+			DPNBlock(size_conv_c + split_size, size_conv_a, size_conv_b, size_conv_c, split_size) for _ in range(num_of_blocks - 1)
+		])
+		self.checkpoint = checkpoint
 
+	def custom_checkpoint_call(self, module):
+		def custom_forward(*inputs):
+			inputs = module(inputs[0])
+			return inputs
+		return custom_forward
+	
 	def forward(self, x):
-		max_pooled = self.max_pooling(x)
-		output = self.initDPN(max_pooled)
-		for _ in range(self.num_of_blocks - 1):
-			output = self.nextDPN(output)
-		return output
-
+		x = self.max_pooling(x)
+		x = self.initDPN(x)
+		for i in range(self.num_of_blocks - 1):
+			if self.checkpoint and ( (i + 2) % round(self.num_of_blocks ** 0.5) ) == 0:
+				x = checkpoint.checkpoint(self.custom_checkpoint_call(self.nextDPN[i]), x, use_reentrant=False)
+			else:
+				x = self.nextDPN[i](x)
+		return x