d58644be5ab1f7debf178e2802e1aed3652fec2b
edbertekaputera <edbertekaputera@gmail.com>
2024-03-26 00:33:23 +0800

    added residual unet v2

--- models/ResidualUnet/ResidualUnet.py
+++ models/ResidualUnet/ResidualUnet.py
@@ -30,3 +30,34 @@ class ResidualUnet(nn.Module):
 		x = self.out_conv(x)
 		x = torch.sigmoid(x)
 		return x
+
+class ResidualUnet_V2(nn.Module):
+	def __init__(self, n_channels) -> None:
+		super(ResidualUnet_V2, self).__init__()
+
+		self.in_conv = DoubleConv(n_channels, 64)
+		self.out_conv = OutConv(64, 1)
+
+		self.down1 = ResidualDownLayer_V2(64, 64, 3)
+		self.down2 = ResidualDownLayer_V2(64, 128, 4)
+		self.down3 = ResidualDownLayer_V2(128, 256, 6)
+		self.down4 = ResidualDownLayer_V2(256, 512, 3)
+		
+		self.up4 = ResidualUpLayer_V2(512 + 256, 256)
+		self.up3 = ResidualUpLayer_V2(256 + 128, 128)
+		self.up2 = ResidualUpLayer_V2(128 + 64, 64)
+		self.up1= ResidualUpLayer_V2(64 + 64, 64)
+
+	def forward(self, x):
+		x0 = self.in_conv(x)
+		x1 = self.down1(x0)
+		x2 = self.down2(x1)
+		x3 = self.down3(x2)
+		x4 = self.down4(x3)
+		x = self.up4(x4, x3)
+		x = self.up3(x, x2)
+		x = self.up2(x, x1)
+		x = self.up1(x, x0)
+		x = self.out_conv(x)
+		x = torch.sigmoid(x)
+		return x
--- models/ResidualUnet/components.py
+++ models/ResidualUnet/components.py
@@ -10,12 +10,12 @@ class IdentityResidualBlock(nn.Module):
 		self.conv1 = nn.Sequential(
 			nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, stride=1),
 			nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True)
 		)
 
 		self.conv2 = nn.Sequential(
 			nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, stride=1),
-			nn.BatchNorm2d(out_channels),
+			nn.BatchNorm2d(out_channels)
 		)
 		
 		self.relu = nn.ReLU(inplace=True)
@@ -25,6 +25,30 @@ class IdentityResidualBlock(nn.Module):
 		x1 = self.conv2(x1)
 		x1 = torch.add(x1, x)
 		x1 = self.relu(x1)
+		return x1
+	
+class IdentityResidualBlock_V2(nn.Module):
+	def __init__(self, in_channels, out_channels, mid_channels=None) -> None:
+		super().__init__()
+		if not  mid_channels:
+			mid_channels = out_channels
+		
+		self.conv1 = nn.Sequential(
+			nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+			nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, stride=1)
+		)
+
+		self.conv2 = nn.Sequential(
+			nn.BatchNorm2d(out_channels),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, stride=1)
+		)
+	
+	def forward(self, x):
+		x1 = self.conv1(x)
+		x1 = self.conv2(x1)
+		x1 = torch.add(x1, x)
 		return x1
 
 class ProjectionResidualBlock(nn.Module):
@@ -36,17 +60,17 @@ class ProjectionResidualBlock(nn.Module):
 		self.conv1 = nn.Sequential(
 			nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, stride=stride),
 			nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True)
 		)
 
 		self.conv2 = nn.Sequential(
 			nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, stride=1),
-			nn.BatchNorm2d(out_channels),
+			nn.BatchNorm2d(out_channels)
 		)
 
 		self.conv_shortcut = nn.Sequential(
 			nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0, stride=stride),
-			nn.BatchNorm2d(out_channels),
+			nn.BatchNorm2d(out_channels)
 		)
 		
 		self.relu = nn.ReLU(inplace=True)
@@ -57,6 +81,38 @@ class ProjectionResidualBlock(nn.Module):
 		x = self.conv2(x)
 		x = torch.add(x, shortcut)
 		x = self.relu(x)
+		return x
+	
+class ProjectionResidualBlock_V2(nn.Module):
+	def __init__(self, in_channels, out_channels, mid_channels=None, stride=2) -> None:
+		super().__init__()
+		if not  mid_channels:
+			mid_channels = out_channels
+		
+		self.conv1 = nn.Sequential(
+			nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),			
+			nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, stride=stride)
+		)
+
+		self.conv2 = nn.Sequential(
+			nn.BatchNorm2d(out_channels),
+			nn.ReLU(inplace=True),	
+			nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, stride=1)
+		)
+
+		self.conv_shortcut = nn.Sequential(
+			nn.BatchNorm2d(out_channels),
+			nn.ReLU(inplace=True),	
+			nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0, stride=stride)
+		)
+		
+
+	def forward(self, x):
+		shortcut = self.conv_shortcut(x)
+		x = self.conv1(x)
+		x = self.conv2(x)
+		x = torch.add(x, shortcut)
 		return x
 	
 class ResidualDownLayer(nn.Module):
@@ -75,11 +131,38 @@ class ResidualDownLayer(nn.Module):
 			x = id_block(x)
 		return x
 
+class ResidualDownLayer_V2(nn.Module):
+	def __init__(self, in_channels, out_channels, reps) -> None:
+		super().__init__()
+
+		self.proj = ProjectionResidualBlock_V2(in_channels, out_channels, stride=2)
+
+		self.id = nn.ModuleList([
+			IdentityResidualBlock_V2(in_channels=out_channels, out_channels=out_channels) for _ in range(reps - 1)
+		])
+	
+	def forward(self, x):
+		x = self.proj(x)
+		for id_block in self.id:
+			x = id_block(x)
+		return x
+
 class ResidualUpLayer(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         self.proj = ProjectionResidualBlock(in_channels, out_channels, stride=1)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x = torch.cat([x2, x1], dim=1)
+        return self.proj(x)
+	
+class ResidualUpLayer_V2(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.proj = ProjectionResidualBlock_V2(in_channels, out_channels, stride=1)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
